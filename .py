5d74cdb64f861a3e3c275c984d9fdf2cb0e0adfd
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-11-08 08:44:10 -0500

    Updated from Brython Server: 11/8/2018 8:42:58 AM

--- .py
+++ .py
@@ -1,11 +1,11 @@
-from math import function
-
+#from math import function
 
 equation = input("what equation would you like me to analize")
 x1 = int(input("on what interval? start value:"))
 x2 = int(input("on what interval? end value:"))
+step = input("what do you want the step to be")
 
-func = function(equation)
+#func = function(equation)
 
 interval = []
 
@@ -13,7 +13,7 @@ for i in range(x1,x2+1):
     if i == x2:
         interval.append(i+.0)
     else:
-        for m in [.0,.1,.2,.3,.4,.5,.6,.7,.8,.9]:
+        for m in int(step)*[0,1,2,3,4,5,6,7,8,9]:
             #print(i+m)
             interval.append(i+m)
 
@@ -27,7 +27,7 @@ for m in equation:
         f.append(part)
         part = []'''
 
-f1  = lambda h: ((h-(-(-h)))/(2*h))
+f1  = lambda a: ((a+.01)-(a-.01))/(2*.01)
 
 for m in interval:
     print(f1(m))
