8762682173b405f4a32b17e21f4fce20602ff95d
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-11-08 08:50:23 -0500

    Updated from Brython Server: 11/8/2018 8:49:12 AM

--- .py
+++ .py
@@ -13,7 +13,7 @@ for i in range(x1,x2+1):
     if i == x2:
         interval.append(i+.0)
     else:
-        for m in int(step)*[0,1,2,3,4,5,6,7,8,9]:
+        for m in [.0,.1,.2,.3,.4,.5,.6,.7,.8,.9]:
             #print(i+m)
             interval.append(i+m)
 
