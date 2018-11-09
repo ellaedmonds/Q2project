eeb571ba5e942797b97b43e6cb6cd772a1ee3598
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-11-09 11:50:03 -0500

    Updated from Brython Server: 11/9/2018 11:48:47 AM

--- .py
+++ .py
@@ -1,9 +1,9 @@
 #from math import function
 
-equation = input("what equation would you like me to analize")
+function = input("what equation would you like me to analize")
 x1 = int(input("on what interval? start value:"))
 x2 = int(input("on what interval? end value:"))
-step = input("what do you want the step to be")
+#step = input("what do you want the step to be")
 
 #func = function(equation)
 
@@ -17,6 +17,41 @@ for i in range(x1,x2+1):
             #print(i+m)
             interval.append(i+m)
 
+a1 = []
+for i in interval:
+    a1.append(i+.01)
+
+a2 = []
+for i in interval:
+    a2.append(i-.01)
+    
+ycoordlist=[]
+for r in interval:
+    x=r
+    Locfunction=function.lower()
+    y=eval(Locfunction)
+    ycoordlist.append(y)
+    
+print(ycoordlist)
+
+ycoordlist1=[]
+for r in a1:
+    x=r
+    Locfunction=function.lower()
+    y=eval(Locfunction)
+    ycoordlist1.append(y)
+    
+print(ycoordlist)
+
+ycoordlist2=[]
+for r in a2:
+    x=r
+    Locfunction=function.lower()
+    y=eval(Locfunction)
+    ycoordlist2.append(y)
+    
+print(ycoordlist)
+
 '''f = []
 part = []
 
@@ -27,7 +62,8 @@ for m in equation:
         f.append(part)
         part = []'''
 
-f1  = lambda a: ((a+.01)-(a-.01))/(2*.01)
+#f1  = lambda a: (ycoorddict[(a+.01)]-ycoorddict[(a-.01)])/(2*.01)
 
 for m in interval:
-    print(f1(m))
+    deriv  = ((ycoordlist1[m])-(ycoordlist2[m]))/(2*0.01)
+    print(deriv)
